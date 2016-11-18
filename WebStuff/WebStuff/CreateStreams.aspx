49ae368a288337e743b0accc2a117f342eeb7a6e
Ian Chapman <ian.chapman@centegra.co.uk>
2016-11-18 12:56:48 +0000

    File and Streams stuff

---
+++ WebStuff/WebStuff/CreateStreams.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="CreateStreams.aspx.vb" Inherits="WebStuff.CreateStreams" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button runat="server" ID="btnExecute" Text="Execute" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebStuff/WebStuff/CreateStreams.aspx.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Data.SqlClient
+Public Class CreateStreams
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Public Sub ExecuteSQL(ByVal SQLString As String)
+        Using connection As New SqlConnection("server=HPV4SQLDEV;uid=sa;pwd=RadianT1;database=Centegra_IC_DEV")
+            Dim command As New SqlCommand(SQLString, connection)
+            command.Connection.Open()
+            command.ExecuteNonQuery()
+        End Using
+    End Sub
+
+    Private Sub btnExecute_Click(sender As Object, e As EventArgs) Handles btnExecute.Click
+        Dim CountInTable As Integer = 2135
+
+        Dim NumberOfStreams As Integer = 4
+
+        Dim myConnection As New SqlConnection("server=HPV4SQLDEV;uid=sa;pwd=RadianT1;database=Centegra_IC_DEV")
+        myConnection.Open()
+        Dim MyCommand As New SqlCommand("SELECT TransactionID FROM ETL_V2_Transactions", myConnection)
+        Dim myReader As SqlDataReader = MyCommand.ExecuteReader()
+        While myReader.Read()
+            ExecuteSQL("UPDATE ETL_V2_Transactions SET StreamID = 1 WHERE TransactionID = " & myReader("TransactionID"))
+        End While
+        myReader.Close()
+        myReader.Dispose()
+        MyCommand.Dispose()
+        myConnection.Close()
+        myConnection.Dispose()
+    End Sub
+End Class
